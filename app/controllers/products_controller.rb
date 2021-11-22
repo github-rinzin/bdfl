963f496778150f860358ad674ada87ef2282c81e
Rinzin Dorji <nizirdorji945@gmail.com>
2021-11-23 00:26:18 +0600

    product scaffolding done, image adding working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,7 +22,8 @@ class ProductsController < ApplicationController
   # POST /products or /products.json
   def create
     @product = Product.new(product_params)
-
+    # user.avatar.attach(params[:avatar])
+    # @product.image.attach(params[:image])
     respond_to do |format|
       if @product.save
         format.html { redirect_to @product, notice: "Product was successfully created." }
@@ -64,6 +65,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:name, :price, :quantity)
+      params.require(:product).permit(:name, :price, :quantity, :image)
     end
 end
